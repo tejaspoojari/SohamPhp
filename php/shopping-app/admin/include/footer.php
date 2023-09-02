9b6285b72d19e2c2611c9899eb36b23d7d365070
tejaspoojari <93251382+tejaspoojari@users.noreply.github.com>
2023-09-02 21:00:12 +0530

    First commit

---
+++ php/shopping-app/admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2023 Online Construction Material Supplier </b> All rights reserved.
+		</div>
+	</div>
---
+++ php/shopping-app/includes/brands-slider.php
@@ -0,0 +1,89 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">Our Brands</h3>
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/ACC.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/ultratech.jpg"height="90" width="120" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/ambuja.jpg"height="100" width="100" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/birlaa.jpg"height="90" width="100" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/jk.jpg"height="100" width="120" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/asian.jpg"height="100" width="120" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/rajuri.jpg" height="100" width="100" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/kalika.jpg" height="100" width="120" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+
+
+
+		    </div><!-- /.owl-carousel #logo-slider -->
+		</div><!-- /.logo-slider-inner -->
+	
+</div><!-- /.logo-slider -->
